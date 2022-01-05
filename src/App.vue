f9a9e8c5ef2a0ba15cf225ca214920944308feab
Jannik Günther <jannik.guenther@lgln.niedersachsen.de>
2022-01-05 14:35:40 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <name-generator/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NameGenerator from './components/NameGenerator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NameGenerator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+.main {
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+  }
 </style>
---
+++ src/components/NameGenerator.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card mb-3">
+        <div class="card-body">
+            {{name}}
+        </div>
+    </div>
+    <div>
+        <button type="button" class="btn btn-primary btn-lg" @click="generate()">Generate</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NameGenerator",
+    data: function() {
+        return {
+            name: "Click on Generate",
+            animals: ["Bear", "Giraffe", "Penguin", "Dog", "Shark"],
+            adjectives: ["bald", "goofy", "prepared", "dangerous", "serious", "honorful"]
+        }
+    },
+    methods: {
+        generate() {
+            // Implement Random logic here
+            // Finish function by setting the name property to the generated name
+            this.name = "This should be a variable with the generated name";
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card {
+    background-color: #313340;
+    color: #eaeaeb;
+}
+</style>
