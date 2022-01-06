16cf6014ad911ffb162c8e7a599ba56fe5a599e8
Steffen Wellegehausen <steffen.wellegehausen@lgln.niedersachsen.de>
2022-01-06 13:10:27 +0100

    adding randomgen function and displaying adjective and animal

--- src/components/NameGenerator.vue
+++ src/components/NameGenerator.vue
@@ -1,37 +1,55 @@
 <template>
-    <div class="card mb-3">
-        <div class="card-body">
-            {{name}}
-        </div>
+  <div class="card mb-3">
+    <div class="card-body">
+      {{ name }}
     </div>
-    <div>
-        <button type="button" class="btn btn-primary btn-lg" @click="generate()">Generate</button>
-    </div>
+  </div>
+  <div>
+    <button type="button" class="btn btn-primary btn-lg" @click="generate()">
+      Generate
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "NameGenerator",
-    data: function() {
-        return {
-            name: "Click on Generate",
-            animals: ["Bear", "Giraffe", "Penguin", "Dog", "Shark"],
-            adjectives: ["bald", "goofy", "prepared", "dangerous", "serious", "honorful"]
-        }
+  name: "NameGenerator",
+  data: function () {
+    return {
+      name: "Click on Generate",
+      animals: ["Bear", "Giraffe", "Penguin", "Dog", "Shark"],
+      adjectives: [
+        "bald",
+        "goofy",
+        "prepared",
+        "dangerous",
+        "serious",
+        "honorful",
+      ],
+    };
+  },
+  methods: {
+    generate() {
+      // Implement Random logic here
+      // Finish function by setting the name property to the generated name
+      this.name =
+        this.adjectives[this.randomgen(this.adjectives)] +
+        " " +
+        this.animals[this.randomgen(this.animals)];
     },
-    methods: {
-        generate() {
-            // Implement Random logic here
-            // Finish function by setting the name property to the generated name
-            this.name = "This should be a variable with the generated name";
-        }
-    }
-}
+    randomgen(array) {
+      //Generate random number with array length
+      let rand = Math.floor(Math.random() * array.length);
+      return rand;
+    },
+    letterlist() {},
+  },
+};
 </script>
 
 <style scoped>
 .card {
-    background-color: #313340;
-    color: #eaeaeb;
+  background-color: #313340;
+  color: #eaeaeb;
 }
 </style>
